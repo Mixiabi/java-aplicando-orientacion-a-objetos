e4454a609f51261a946c886ecb985e62351aa935
Mxiabi1996 <michelle_montoya96@outlook.com>
2024-05-10 13:50:47 -0500

    agregando funciones y metodos

--- src/Pelicula.java
+++ src/Pelicula.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Pelicula {
+public class Pelicula {
+    String nombre;
+    int fecLanzamiento;
+    int duracionMinutos;
+    boolean planIncluido;
+    double sumaPuntuaciones;
+    int totalPuntuaciones;
+
+    void fichaTecnica(){
+        System.out.println("Pelicula: " + nombre);
+        System.out.println("Año de fecLanzamiento: " + fecLanzamiento);
+        System.out.println("Incluido en el Plan: " + planIncluido);
+    }
+
+    void evaluacion(double nota){
+
+        sumaPuntuaciones = sumaPuntuaciones + nota;
+        totalPuntuaciones++;
+    }
+
+    double mediaPuntuaciones(){
+        return sumaPuntuaciones/totalPuntuaciones;
+
+    }
 }
--- src/Principal.java
+++ src/Principal.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Principal {
+public class Principal {
+    public static void main(String[] args){
+
+        Pelicula miPelicula = new Pelicula();
+
+        miPelicula.nombre = "Matrix";
+        miPelicula.fecLanzamiento = 1998;
+        miPelicula.duracionMinutos = 120;
+        miPelicula.fichaTecnica();
+        miPelicula.evaluacion(5);
+        miPelicula.evaluacion(2.3);
+        miPelicula.evaluacion(6.1);
+        miPelicula.evaluacion(4.2);
+        miPelicula.evaluacion(1.8);
+        System.out.println("Suma de puntuaciones es: " + miPelicula.sumaPuntuaciones);
+        System.out.println("Total de puntuaciones es: " + miPelicula.totalPuntuaciones);
+        System.out.println("La media de evaluacion es de: " + miPelicula.mediaPuntuaciones());
+
+
+    }
 }
